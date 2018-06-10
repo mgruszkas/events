e1a0c34b48e6dbc471514d51d1b6d08a820d8bbb
michal <m9ruszka@gmail.com>
2018-06-10 13:17:06 +0200

    Code refactoring

--- src/providers/events/events.ts
+++ src/providers/events/events.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Event, EventMember } from './../../models';
+import { Event } from './../../models';
 import { map } from 'rxjs/operators';
 
 export * from './../../models/Event';
